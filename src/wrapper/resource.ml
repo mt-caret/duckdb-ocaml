dbeba933a4c8f6a5f10cd195cb96e52ad7c5acaf
mtakeda <mtakeda@oeconomia.jp>
2025-02-10 13:29:04 +0000

    attach finalisers to [Resource.t]s

--- src/wrapper/resource.ml
+++ src/wrapper/resource.ml
@@ -1,24 +1,36 @@
 open! Core
 
+module State = struct
+  type 'a t =
+    | Available of 'a
+    | First_freed_at of Source_code_position.t
+end
+
 type 'a t =
-  { mutable resource : ('a, Source_code_position.t) result
+  { mutable resource : 'a State.t
   ; name : string
   ; free : 'a -> unit
   }
 
-let create resource ~name ~free = { resource = Ok resource; name; free }
-
 let get_exn t =
   match t.resource with
-  | Error first_freed_at ->
+  | First_freed_at first_freed_at ->
     raise_s [%message "Already freed" t.name (first_freed_at : Source_code_position.t)]
-  | Ok resource -> resource
+  | Available resource -> resource
 ;;
 
-let free t ~here =
+let free' t ~here =
   match t.resource with
-  | Error _ -> ()
-  | Ok resource ->
+  | First_freed_at _ -> ()
+  | Available resource ->
     t.free resource;
-    t.resource <- Error here
+    t.resource <- First_freed_at here
 ;;
+
+let create resource ~name ~free =
+  let t = { resource = Available resource; name; free } in
+  Gc.Expert.add_finalizer_exn t (fun t -> free' t ~here:[%here]);
+  t
+;;
+
+let free = free'
--- src/wrapper/resource.mli
+++ src/wrapper/resource.mli
@@ -7,7 +7,11 @@ type 'a t
 
 val create : 'a -> name:string -> free:('a -> unit) -> 'a t
 
-(** Raises an exception if the resource has already been freed. *)
+(** Raises an exception if the resource has already been freed. 
+
+    When [t] is garbage collected, a finalizer will call [free] to avoid memory
+    leaks. As a result, callers of [get_exn] should not (e.g. store the returned
+    value in a record) since the underlying resource may be freed at any time. *)
 val get_exn : 'a t -> 'a
 
 val free : 'a t -> here:Source_code_position.t -> unit
